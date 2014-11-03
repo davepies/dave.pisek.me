cc8afd14b01c78e9b93c9eab79322bea4dac08ed
Dave Pisek <dave.pisek@gmail.com>
2014-11-03 22:49:04 +1100

    Only opens read more once.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,17 +1,33 @@
 (function () {
 
-    var readMoreContainers = document.querySelectorAll('.read-more');
+    function Toggler (options) {
+        options = options || {};
+        this.className = options.className || '.read-more';
+        this.classNameShowing = options.classNameShowing || this.className + '--showing';
 
-    var showingClass = 'read-more--showing';
-
-    function toggle (el) {
-        return function () {
-            el.classList[el.classList.contains(showingClass) ? 'remove' : 'add'](showingClass);
-        };
+        this.readMoreContainers = document.querySelectorAll(this.className);
     }
 
-    Array.prototype.slice.call(readMoreContainers).forEach(function (el) {
-        el.addEventListener('click', toggle(el));
-    });
+    Toggler.prototype.toggle = function (el) {
+        var showingClass = this.classNameShowing.replace('.', '');
+        el.classList[el.classList.contains(showingClass) ? 'remove' : 'add'](showingClass);
+    };
+
+    Toggler.prototype.bindToggle = function () {
+        Array.prototype.slice.call(this.readMoreContainers).forEach(function (el) {
+            this.one(el, 'click', this.toggle.bind(this, el));
+        }.bind(this));
+    };
+
+    Toggler.prototype.one = function (el, eventName, eventHandler, context) {
+        el.addEventListener(eventName, function handleOnce () {
+            eventHandler.call(context || null);
+            el.removeEventListener(eventName, handleOnce);
+        });
+    }
+
+
+    var toggler = new Toggler();
+    toggler.bindToggle();
 
 })();
