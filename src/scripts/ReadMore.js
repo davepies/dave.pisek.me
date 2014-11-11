f7756ee4332e72ddffca0a61b1192f3efca2c6f7
Dave Pisek <dave.pisek@gmail.com>
2014-11-11 22:06:29 +1100

    Adds merge for default options.

--- src/scripts/ReadMore.js
+++ src/scripts/ReadMore.js
@@ -1,36 +1,44 @@
+var merge = require('merge');
+
+var defaults = {
+    sel: '[am-read-more]',
+    attrShowingValue: 'showing'
+};
+
+
 function ReadMore (options) {
 
-    this.options = options || {};
-    this.sel = options.sel || '[am-read-more]';
-    this.attrShowingValue = options.attrShowingValue || 'showing';
+    options = options || {};
 
-    this.readMoreContainers = document.querySelectorAll(this.sel);
+    this.options = merge(options, defaults);
 
-    this._addListeners();
+    this.readMoreContainers = document.querySelectorAll(this.options.sel);
+
+    this._attachListeners();
 
 }
 
 
 ReadMore.prototype.toggle = function (el) {
 
-    var attr = this._normaliseSel(this.sel);
-    var newAttrValue = (el.getAttribute(attr) === this.attrShowingValue) ? '' : this.attrShowingValue;
+    var attr = this._normaliseSel(this.options.sel);
+    var newAttrValue = (el.getAttribute(attr) === this.options.attrShowingValue) ? '' : this.options.attrShowingValue;
 
     el.setAttribute(attr, newAttrValue);
 
 };
 
 
-ReadMore.prototype._addListeners = function () {
+ReadMore.prototype._attachListeners = function () {
 
     Array.prototype.slice.call(this.readMoreContainers).forEach(function (el) {
-        this._addListener(el, 'click', this.toggle.bind(this, el), this.options.once);
+        this._attachListener(el, 'click', this.toggle.bind(this, el), this.options.once);
     }.bind(this));
 
 };
 
 
-ReadMore.prototype._addListener = function (el, eventName, eventHandler, context, once) {
+ReadMore.prototype._attachListener = function (el, eventName, eventHandler, context, once) {
 
     el.addEventListener(eventName, function handler () {
         eventHandler.call(context || null);
